2a4ea6f54c4ff98ab778c814ca28041491805d5d
Natasha Paneva <59098617+natepaneva@users.noreply.github.com>
2020-03-07 23:19:12 +0100

    Traffic Lights

---
+++ Homework1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("STOP");
+            System.Threading.Thread.Sleep(1000);
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("READY");
+            System.Threading.Thread.Sleep(1000);
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("GO");
+            System.Threading.Thread.Sleep(1000);
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
